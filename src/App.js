d84b08a60cec93b5aa0175b352b65709e4ab5825
Sharon Wasker <sharonwasker@gmail.com>
2020-12-09 16:13:12 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>shron's TODO</h1>
+        <h1>Sharon's TODO</h1>
       </header>
     </div>
   );
